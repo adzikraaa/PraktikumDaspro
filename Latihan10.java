7072349bd2258ff74642b1d12e45ae461ff139c2
adzikraaa <dzikra.zen@gmail.com>
2024-09-06 09:55:28 -0700

    commitPraktikumDaspro

---
+++ Latihan10.java
@@ -0,0 +1,15 @@
+/**
+ * Latihan10
+ */
+public class Latihan10 {
+
+    public static void main(String[] args) {
+        System.out.println("**** IDENTITAS DIRI ****");
+        System.out.println("Nama     : Dzikra");
+        System.out.println("Absen    : 10");
+        System.out.println("Kelas    : 1D");
+        System.out.println("Prodi    : Sistem Informasi Bisnis");
+        System.out.println("Jurusan  : Teknologi Informasi");
+        
+    }
+    }
