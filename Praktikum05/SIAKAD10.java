4288f92d571dfc8f5fa272626570337395e8c056
adzikraaa <dzikra.zen@gmail.com>
2024-09-26 20:23:33 -0700

    Praktikum05

---
+++ Praktikum05/SIAKAD10.java
@@ -0,0 +1,11 @@
+package Praktikum05;
+
+import java.util.Scanner;
+
+public class SIAKAD10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+    }
+
+}
