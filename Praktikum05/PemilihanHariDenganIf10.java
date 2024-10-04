7ce9ea1b2e4582a3185b1799bbf697be1dc403a0
adzikraaa <dzikra.zen@gmail.com>
2024-10-03 18:06:39 -0700

    Praktikum06

--- Praktikum05/PemilihanHariDenganIf10.java
+++ Praktikum05/PemilihanHariDenganIf10.java
@@ -1,7 +1,7 @@
 package Praktikum05;
 import java.util.Scanner;
 
-public class PemilihanHariDenganNoAbsen {
+public class PemilihanHariDenganIf10 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
---
+++ Praktikum06/Pemilihan2Percobaan110.java
@@ -0,0 +1,23 @@
+package Praktikum06;
+
+
+
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan110 {
+    public static void main(String[] args) {
+       Scanner sc10 = new Scanner(System.in);
+
+       
+       System.out.print("Masukkan tahun: ");
+       int tahun = sc10.nextInt();
+
+       if ((tahun % 4) == 0) {
+            if ((tahun % 100) != 0)
+            System.out.println("Tahun Kabisat");
+        } else
+            System.out.println("Bukan tahun kabisat" ); 
+
+
+    }
+}
